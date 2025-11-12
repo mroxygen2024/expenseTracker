470558685199717464421d921f22addcca5ab504
Fuad Sano Kedir <fuadsk@localhost.localdomain>
2025-11-12 16:39:04 +0300

    feat: auth feature added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-// import prisma from "./utils/prismaClient.ts";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import expenseRoutes from "./routes/expenses.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  res.send("Expense Tracker Backend is running ✅");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
