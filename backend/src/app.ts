aa391192dc3db783e6ad8054d8b0307466b53988
Fuad Sano Kedir <fuadsk@localhost.localdomain>
2025-11-15 11:17:44 +0300

    fix: cors error fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,10 @@ import { setupSwagger } from "./utils/swagger.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+   origin: "http://localhost:5000", // allow Vite frontend
+   credentials: true
+}));
 
 
 setupSwagger(app);
